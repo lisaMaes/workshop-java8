6e05bb27525a4b87d0677674bf003bb6da3e4489
Lisa <maestrilisa@yahoo.fr>
2019-02-18 16:43:35 +0100

    ajout des tps sur date time except exo5

--- src/test/java/java8/ex02/DateAndTime_02_Test.java
+++ src/test/java/java8/ex02/DateAndTime_02_Test.java
@@ -76,7 +76,7 @@ public class DateAndTime_02_Test {
         LocalDate localDate = LocalDate.of(2015, 03, 11);
 
         // TODO Formatter la date pour avoir l'affichage suivant : "11/03/2015 00:00:00"
-        localDate.format(null);
+        String result = localDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy H:m:s"));
     }
 
     @Test
@@ -84,10 +84,11 @@ public class DateAndTime_02_Test {
 
         // TODO créer un objet LocalDate à la date 10/01/2000
         // TODO utiliser la méthode of
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2000, 01, 10);
+
 
         // TODO transformer la date précédente en 05/02/2015
-        LocalDate result = null;
+        LocalDate result = localDate.withYear(2015).withMonth(02).withDayOfMonth(05);
 
         assertThat(result.getYear(), is(2015));
         assertThat(result.getMonth(), is(Month.FEBRUARY));
